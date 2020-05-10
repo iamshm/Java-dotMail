e5370595c2e3fa710d0c5f04fe79d6bce7bb850c
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-05-11 01:30:31 +0530

    First commit

---
+++ src/com/iamshm/dotMail/sendMail.java
@@ -0,0 +1,42 @@
+
+package com.iamshm.dotMail;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class sendMail {
+    private String user = "msuraj0022@gmail.com";
+    private String pass ="R3p1ic@te";
+    
+    sendMail(String to,String sub, String msg){
+        Properties prop = new Properties();
+        prop.put("mail.smtp.ssl.trust","smtp.gmail.com");
+        prop.put("mail.smtp.auth",true);
+        prop.put("mail.smtp.starttls.enable",true);
+        prop.put("mail.smtp.host","smtp.gmail.com");
+        prop.put("mail.smtp.port","587");
+        
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+              protected javax.mail.PasswordAuthentication getPasswordAuthentication(){
+               return new javax.mail.PasswordAuthentication(user,pass);
+              }
+                });
+        try{
+           Message message = new MimeMessage(session) ;
+           message.setFrom(new InternetAddress("no-reply@gmail.com"));
+           message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+           message.setSubject(sub);
+           message.setText(msg);
+           Transport.send(message);
+           System.out.println("Mail Sent");
+        }
+       catch(Exception e){
+           
+            System.out.println(e);
+       }
+       }
+}
